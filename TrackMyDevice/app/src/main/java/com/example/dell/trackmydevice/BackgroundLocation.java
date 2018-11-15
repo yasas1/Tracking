5cf7ccf4df8505a7b8f70ff76ea3cd82535a1507
yasas1 <32437531+yasas1@users.noreply.github.com>
2018-11-15 10:15:53 +0530

    updated background class

---
+++ TrackMyDevice/app/src/main/java/com/example/dell/trackmydevice/BackgroundLocation.java
@@ -0,0 +1,25 @@
+package com.example.dell.trackmydevice;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Dell on 11/15/2018.
+ */
+
+public class BackgroundLocation extends AsyncTask<Void,Void,Void> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
